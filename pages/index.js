7417197009167f85654ddf95758c396ed29c00f4
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-06 10:26:30 -0800

    float button to the right

--- pages/index.js
+++ pages/index.js
@@ -28,12 +28,13 @@ export default class CampaignIndex extends Component {
         <div>
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
           <h3> Open Campaigns </h3>
-          { this.renderCampaigns() }
           <Button
+            floated="right"
             content="Create Campaign"
             icon="add"
             primary
           />
+          { this.renderCampaigns() }
         </div>
       </Layout>
     )
