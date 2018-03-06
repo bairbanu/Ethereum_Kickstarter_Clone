793d059b0a20ba96099411573270272a0e956882
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-06 10:03:33 -0800

    add open campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Card, Button } from 'semantic-ui-react'
 import factory from '../ethereum/factory'
 
 export default class CampaignIndex extends Component {
@@ -8,7 +9,30 @@ export default class CampaignIndex extends Component {
     return { campaigns }
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a> View Campaign </a>,
+        fluid: true
+      }
+    })
+
+    return <Card.Group items={ items } />
+  }
+
   render() {
-    return <h1> { this.props.campaigns } </h1>
+    return (
+      <div>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+        <h3> Open Campaigns </h3>
+        { this.renderCampaigns() }
+        <Button
+          content="Create Campaign"
+          icon="add"
+          primary
+        />
+      </div>
+    )
   }
 }
