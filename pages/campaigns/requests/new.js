48c1ddb9b7563860304af5f7503c4a01447b66b3
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-08 09:33:58 -0800

    add create request button

---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Form, Button, Message } from 'semantic-ui-react'
+
+export default class RequestNew extends Component {
+  render() {
+    return (
+      <h3> New Request </h3>
+    )
+  }
+}
--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ routes
   .add('/campaigns/new', '/campaigns/new')
   .add('/campaigns/:address', '/campaigns/show')
   .add('/campaigns/:address/requests', '/campaigns/requests/index')
+  .add('/campaigns/:address/requests/new', '/campaigns/requests/new')
 
 module.exports = routes
