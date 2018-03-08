4b2ff5f3c2b71468c171731dc301a0a741ef6651
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-07 16:26:36 -0800

    render contribution form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Grid } from 'semantic-ui-react'
 
 import Layout from '../../components/Layout'
 import campaignInstance from '../../ethereum/campaign'
 import web3 from '../../ethereum/web3'
+import ContributeForm from '../../components/contributeForm'
 
 export default class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -12,6 +13,7 @@ export default class CampaignShow extends Component {
     const summary = await campaign.methods.getSummary().call()
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -65,7 +67,16 @@ export default class CampaignShow extends Component {
     return (
       <Layout>
         <h1> Campaign Dashboard </h1>
-        { this.renderCards() }
+
+        <Grid>
+          <Grid.Column width={ 10 }>
+            { this.renderCards() }
+          </Grid.Column>
+
+          <Grid.Column width={ 6 }>
+            <ContributeForm address={ this.props.address }/>
+          </Grid.Column>
+        </Grid>
       </Layout>
     )
   }
