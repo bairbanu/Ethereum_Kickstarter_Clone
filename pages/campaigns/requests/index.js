3085ee59031b863aa963d7e134840db6c75044b0
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-08 19:10:29 -0800

    get requests from smart contract

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,11 +3,21 @@ import { Button } from 'semantic-ui-react'
 
 import { Link } from '../../../routes'
 import Layout from '../../../components/Layout'
+import deployedCampaignInstance from '../../../ethereum/campaign'
 
 export default class RequestIndex extends Component {
-  static getInitialProps(props) {
+  static async getInitialProps(props) {
     const { address } = props.query
-    return { address }
+    const campaign = deployedCampaignInstance(address)
+    const requestCount = await campaign.methods.getRequestsCount().call()
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount)).fill().map((element, index) => {
+        return campaign.methods.requests(index).call()
+      })
+    )
+
+    return { address, requests, requestCount }
   }
 
   render() {
