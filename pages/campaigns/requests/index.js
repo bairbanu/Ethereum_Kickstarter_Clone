56de56817f3502f8e843a2910dd9f17a17df88a6
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-08 19:21:31 -0800

    setup to display rows on request

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Table } from 'semantic-ui-react'
 
 import { Link } from '../../../routes'
 import Layout from '../../../components/Layout'
+import RequestRow from '../../../components/RequestRow'
 import deployedCampaignInstance from '../../../ethereum/campaign'
 
 export default class RequestIndex extends Component {
@@ -20,7 +21,19 @@ export default class RequestIndex extends Component {
     return { address, requests, requestCount }
   }
 
+  renderRows() {
+    return this.props.requests.map((request, index) =>
+      <RequestRow
+        key={ index }
+        request={ request }
+        address={ this.props.address }
+      />
+    )
+  }
+
   render() {
+    const { Header, Row, HeaderCell, Body } = Table
+
     return (
       <Layout>
         <h3> Request List </h3>
@@ -29,6 +42,22 @@ export default class RequestIndex extends Component {
             <Button primary> Add Request </Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell> ID </HeaderCell>
+              <HeaderCell> Description </HeaderCell>
+              <HeaderCell> Amount </HeaderCell>
+              <HeaderCell> Recipient </HeaderCell>
+              <HeaderCell> Approval </HeaderCell>
+              <HeaderCell> Approve </HeaderCell>
+              <HeaderCell> Finalize </HeaderCell>
+            </Row>
+          </Header>
+          <Body>
+            { this.renderRows() }
+          </Body>
+        </Table>
       </Layout>
     )
   }
